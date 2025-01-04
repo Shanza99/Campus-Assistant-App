57c45277e55da2ceee532866ed4284e8502f52ed
shanza99 <shanzabatool001@gmail.com>
2025-01-04 18:57:44 +0500

    Firebase Authentication Integration

--- campus/lib/main.dart
+++ campus/lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart'; // Ensure this path is correct
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
