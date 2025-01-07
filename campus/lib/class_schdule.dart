3253cd8ab6831bb7bd4d78498957fffa662ea497
shanza99 <shanzabatool001@gmail.com>
2025-01-07 19:11:51 +0500

    Offline Storage (SQFlite)

--- campus/lib/class_schdule.dart
+++ campus/lib/class_schdule.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class ClassScheduleScreen extends StatefulWidget {
